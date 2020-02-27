34ae9f8088d1aef7bd40201ab1828b13100534a1
christianbrotherson <christian.brotherson@gmail.com>
2020-02-27 14:41:44 -0700

    finished clear form functionality

--- src/components/card.js
+++ src/components/card.js
@@ -3,29 +3,31 @@ import React, { Component } from 'react';
 import Input from './input';
 import Content from './content';
 
+const INITIAL_STATE = {
+    color: '',
+    pluralNoun: '',
+    adjectiveOne: '',
+    celebOne: '',
+    adjectiveTwo: '',
+    nounOne: '',
+    numberOne: '',
+    numberTwo: '',
+    nounTwo: '',
+    adjectiveThree: '',
+    celebTwo: '',
+    celebThree: '',
+    adjectiveFour: '',
+    nounThree: '',
+    celebFour: '',
+    adjectiveFive: '',
+    contentVisible: false
+};
+
 class Card extends Component {
     constructor() {
         super()
 
-        this.state = {
-            color: '',
-            pluralNoun: '',
-            adjectiveOne: '',
-            celebOne: '',
-            adjectiveTwo: '',
-            nounOne: '',
-            numberOne: '',
-            numberTwo: '',
-            nounTwo: '',
-            adjectiveThree: '',
-            celebTwo: '',
-            celebThree: '',
-            adjectiveFour: '',
-            nounThree: '',
-            celebFour: '',
-            adjectiveFive: '',
-            contentVisible: false
-        }
+        this.state = INITIAL_STATE;
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
@@ -37,7 +39,11 @@ class Card extends Component {
 
     handleFormSubmit(event) {
         event.preventDefault();
-        this.setState({ contentVisible: !this.state.contentVisible });
+        if (this.state.contentVisible) {
+            this.setState(INITIAL_STATE);
+        } else {
+            this.setState({ contentVisible: !this.state.contentVisible });
+        }
     }
 
     render () {
@@ -53,8 +59,8 @@ class Card extends Component {
             { title: 'Number', state: this.state.numberOne, name: 'numberOne' },
             { title: 'Number', state: this.state.numberTwo, name: 'numberTwo' },
 
-            { title: 'Celebrity', state: this.state.nounTwo, name: 'nounTwo' },
-            { title: 'Celebrity', state: this.state.adjectiveThree, name: 'adjectiveThree' },
+            { title: 'Noun', state: this.state.nounTwo, name: 'nounTwo' },
+            { title: 'Adjective', state: this.state.adjectiveThree, name: 'adjectiveThree' },
             { title: 'Celebrity', state: this.state.celebTwo, name: 'celebTwo' },
             { title: 'Celebrity', state: this.state.celebThree, name: 'celebThree' },
             
